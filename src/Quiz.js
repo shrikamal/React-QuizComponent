1fabb561c9de6508fe61a046bf9b26be0fef12b5
ShriKamal <shriramkamalakannan3@gmail.com>
2018-07-31 17:31:18 +0530

    Tweak1 for module 2

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-mport QuizQuestion from './QuizQuestion.js';
+import QuizQuestion from './QuizQuestion.js';
 let quizData=require('./quiz_data.json');
 
 class Quiz extends Component{
